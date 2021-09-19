2efc86c00150543615ccaadf4be2eedd7bf99546
jecox85 <13830384+jecox85@users.noreply.github.com>
2021-09-18 23:09:01 -0600

    Started Chapter 5 reading examples.

---
+++ Chapter05/PersonalWork/Ch05Ex02/Ch05Ex02/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Ch05Ex02
+{
+    enum orientation : byte
+    {
+        north = 1,
+        south = 2,
+        east = 3,
+        west = 4
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This is the text example 2 from Chapter 5 for using Enumeration on page 89
+            byte directionByte;
+            string directionString;
+            orientation myDirection = orientation.north;
+            Console.WriteLine($"myDirection = {myDirection}");
+            directionByte = (byte)myDirection;
+            directionString = Convert.ToString(myDirection);
+            Console.WriteLine($"byte equivalent = {directionByte}");
+            Console.WriteLine($"string equivalent = {directionString}");            
+            Console.ReadKey();
+        }
+    }
+}
