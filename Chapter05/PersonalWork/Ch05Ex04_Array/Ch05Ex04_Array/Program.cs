439fbbf5dcf986e36ba55cf9cd847046791a2c56
jecox85 <13830384+jecox85@users.noreply.github.com>
2021-09-19 00:48:21 -0600

    Finished Chapter 5 reading examples.

---
+++ Chapter05/PersonalWork/Ch05Ex04_Array/Ch05Ex04_Array/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ch05Ex04_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Reading example for arrays found on page 96 of the text
+            string[] friendNames = {
+                "Todd Anthony",
+                "Mary Chris",
+                "Autry Rual" 
+            };
+            int i;
+            Console.WriteLine($"Here are {friendNames.Length} of my friends:");
+            for(i = 0; i < friendNames.Length; i++)
+            {
+                Console.WriteLine(friendNames[i]);
+            }
+            Console.WriteLine("Now using a foreach loop.");
+            foreach (string friendName in friendNames)
+            {
+                Console.WriteLine(friendName);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Chapter05/PersonalWork/Ch05Ex06_StatementAutoCompletion/Ch05Ex06_StatementAutoCompletion/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ch05Ex06_StatementAutoCompletion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This is the reading example 6 for chapter 5 about Statement Auto-Completion found on page 108.
+            string myString = "This is a test.";
+            char[] separator = { ' ' };
+            string[] myWords;
+            myWords = myString.Split(separator);
+            foreach (string word in myWords)
+            {
+                Console.WriteLine($"{word}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
