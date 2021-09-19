af55954c3532b6755250096fa6f7fe49ffb6a9fa
jecox85 <13830384+jecox85@users.noreply.github.com>
2021-09-18 23:43:23 -0600

    Renamed Personal Chapter Examples for easier referencing.

---
+++ Chapter03/PersonalWork/Ch03Ex01_TypeVariables/Ch03Ex01_TypeVariables/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ch03Ex01_TypeVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Reading example found on page 37 of the text.
+            int myInteger;
+            string myString;
+            myInteger = 17;
+            myString = "\"myInteger\" is";
+            Console.WriteLine($"{myString} {myInteger}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Chapter04/PersonalWork/Ch04Ex01_BooleanOperators/Ch04Ex01_BooleanOperators/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ch04Ex01_BooleanOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Text example for using Boolean Operators on page 58
+            Console.WriteLine("Enter an integer: ");
+            int myInt = Convert.ToInt32(Console.ReadLine());
+            bool isLessThan10 = myInt < 10;
+            bool isBetween0and5 = (0 <= myInt) && (myInt <= 5);
+            Console.WriteLine($"Integer less than 10? {isLessThan10}");
+            Console.WriteLine($"Integer between 0 and 5? {isBetween0and5}");
+            Console.WriteLine($"Exactly one of the above is true? " + $"{isLessThan10 ^ isBetween0and5}");
+            Console.ReadKey();
+        }
+    }
+}
