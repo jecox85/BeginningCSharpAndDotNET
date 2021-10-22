852fae4acababb0f59ca92c5fb2fda96c20dcc3a
jecox85 <13830384+jecox85@users.noreply.github.com>
2021-10-21 22:41:00 -0600

    Completed chapter 10 exercises.

---
+++ Chapter10/PersonalWork/Exercise10.4/Exercise10.4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Exercise10._4
+{
+    public class MyCopyableClass
+    {
+        public int ClassInt { get; set; }
+        public MyCopyableClass()
+        {
+            ClassInt = 0;
+        }
+        public MyCopyableClass(int val)
+        {
+            this.ClassInt = val;
+        }
+        public MyCopyableClass GetCopy()
+        {
+            return (MyCopyableClass)this.MemberwiseClone();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Creating first object with a ClassInt of 7.");
+            MyCopyableClass objectA = new MyCopyableClass(7);
+            Console.WriteLine("Creating second object and using GetCopy to copy the first object into it.");
+            MyCopyableClass objectB = objectA.GetCopy();
+            Console.WriteLine("Changing the int in the first object so that we can verify both objects " +
+                "don't point to the same place in memory.");
+            objectA.ClassInt = 3;
+            Console.WriteLine($"objectA.ClassInt = " + objectA.ClassInt.ToString() + " objectb.ClassInt = "
+                + objectB.ClassInt.ToString());
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Chapter10/PersonalWork/Exercise10.5/Exercise10.5/Program.cs
@@ -0,0 +1,46 @@
+﻿//This program is to solve exercise 10.5 on page 263 of the text, but I modified it to keep looping until I get a flush.
+using System;
+using Ch10CardLib;
+
+namespace Exercise10._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deck myDeck = new Deck();
+            bool flush = false;
+            while (!flush)
+            {
+                myDeck.Shuffle();
+                for (int i = 0; i < 50; i += 5)
+                {
+                    Card[] tempCards =
+                    {
+                        myDeck.GetCard(i),
+                        myDeck.GetCard(i + 1),
+                        myDeck.GetCard(i + 2),
+                        myDeck.GetCard(i + 3),
+                        myDeck.GetCard(i + 4),
+                    };
+                    if (tempCards[0].suit == tempCards[1].suit &&
+                        tempCards[0].suit == tempCards[2].suit &&
+                        tempCards[0].suit == tempCards[3].suit &&
+                        tempCards[0].suit == tempCards[4].suit)
+                    {
+                        flush = true;
+                        Console.WriteLine("Flush!");
+                        Console.WriteLine(tempCards[0].ToString() + ", " + tempCards[1].ToString() + ", " + tempCards[2].ToString()
+                            + ", " + tempCards[3].ToString() + ", " + tempCards[4].ToString());
+                        break;
+                    }
+                    else if(i == 45)
+                    {
+                        Console.WriteLine("No flush.");
+                    }
+                }                
+            }
+
+        }
+    }
+}
